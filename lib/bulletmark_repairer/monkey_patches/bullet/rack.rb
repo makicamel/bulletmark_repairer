435f2b4b4f78db66bc283842f79b577623e25de3
makicamel <unright@gmail.com>
2023-09-18 07:25:54 +0900

    Use BulletmarkRepairer.notifications to get target file instead of BulletmarkRepairer.tracers

--- lib/bulletmark_repairer/monkey_patches/bullet/rack.rb
+++ lib/bulletmark_repairer/monkey_patches/bullet/rack.rb
@@ -19,14 +19,16 @@ module BulletmarkRepairer
         if BulletmarkRepairer.notifications.present?
           controller_corrector = Pathname.new(__FILE__).sub('/monkey_patches/bullet/rack.rb', '/controller_corrector.rb')
           default_corrector = Pathname.new(__FILE__).sub('/monkey_patches/bullet/rack.rb', '/corrector.rb')
-          tracer = BulletmarkRepairer.tracers.last
+          tracers = BulletmarkRepairer.notifications.last.instance_variable_get(:@callers)
+          n_plus_one_in_view = tracers.any? { |tracer| tracer.match?(%r{\A#{Rails.root}/app/views/[./\w]+:\d+:in `[\w]+'\z}) }
 
-          file_name = tracer&.scan(%r{\A([./\w]+):\d+:in `[\w\s]+'\z})&.flatten&.first
-          if file_name.start_with?("#{Rails.root}/app/views")
+          if n_plus_one_in_view
             BulletmarkRepairer.action = env['action_dispatch.request.parameters']['action'].to_sym
             controller_file = "#{Rails.root}/app/controllers/#{env['action_dispatch.request.parameters']['controller']}_controller.rb"
             Parser::Runner::RubyRewrite.go(%W[-l #{controller_corrector} -m #{controller_file}])
           else
+            tracers.any? { |tracer| tracer =~ %r{\A([./\w]+):\d+:in `[\w\s]+'\z} }
+            file_name = Regexp.last_match[1]
             Parser::Runner::RubyRewrite.go(%W[-l #{default_corrector} -m #{file_name}])
           end
         end
