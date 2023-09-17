ee883ecb1d20b4723b83a2c9c6540eea8205d0ca
makicamel <unright@gmail.com>
2023-09-17 21:20:41 +0900

    Absolute path

--- lib/bulletmark_repairer/monkey_patches/bullet/rack.rb
+++ lib/bulletmark_repairer/monkey_patches/bullet/rack.rb
@@ -17,15 +17,17 @@ module BulletmarkRepairer
         super
       ensure
         if BulletmarkRepairer.notifications.present?
+          controller_corrector = Pathname.new(__FILE__).sub('/monkey_patches/bullet/rack.rb', '/controller_corrector.rb')
+          default_corrector = Pathname.new(__FILE__).sub('/monkey_patches/bullet/rack.rb', '/corrector.rb')
           tracer = BulletmarkRepairer.tracers.last
 
           file_name = tracer&.scan(%r{\A([./\w]+):\d+:in `[\w\s]+'\z})&.flatten&.first
           if file_name.start_with?("#{Rails.root}/app/views")
             BulletmarkRepairer.action = env['action_dispatch.request.parameters']['action'].to_sym
             controller_file = "#{Rails.root}/app/controllers/#{env['action_dispatch.request.parameters']['controller']}_controller.rb"
-            Parser::Runner::RubyRewrite.go(%W[-l lib/bulletmark_repairer/controller_corrector.rb -m #{controller_file}])
+            Parser::Runner::RubyRewrite.go(%W[-l #{controller_corrector} -m #{controller_file}])
           else
-            Parser::Runner::RubyRewrite.go(%W[-l lib/bulletmark_repairer/corrector.rb -m #{file_name}])
+            Parser::Runner::RubyRewrite.go(%W[-l #{default_corrector} -m #{file_name}])
           end
         end
       end
