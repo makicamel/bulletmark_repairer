5195f78f25dfca931d0d1a18d745c34bb548baf4
makicamel <unright@gmail.com>
2023-09-18 16:10:15 +0900

    Refactor

--- lib/bulletmark_repairer/monkey_patches/bullet/rack.rb
+++ lib/bulletmark_repairer/monkey_patches/bullet/rack.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'parser/runner/ruby_rewrite'
-
 module BulletmarkRepairer
   module Bullet
     def end_request
@@ -17,20 +15,10 @@ module BulletmarkRepairer
         super
       ensure
         if BulletmarkRepairer.notifications.present?
-          controller_corrector = Pathname.new(__FILE__).sub('/monkey_patches/bullet/rack.rb', '/controller_corrector.rb')
-          default_corrector = Pathname.new(__FILE__).sub('/monkey_patches/bullet/rack.rb', '/corrector.rb')
-          tracers = BulletmarkRepairer.notifications.last.instance_variable_get(:@callers)
-          n_plus_one_in_view = tracers.any? { |tracer| tracer.match?(%r{\A#{Rails.root}/app/views/[./\w]+:\d+:in `[\w]+'\z}) }
-
-          if n_plus_one_in_view
-            BulletmarkRepairer.action = env['action_dispatch.request.parameters']['action'].to_sym
-            controller_file = "#{Rails.root}/app/controllers/#{env['action_dispatch.request.parameters']['controller']}_controller.rb"
-            Parser::Runner::RubyRewrite.go(%W[-l #{controller_corrector} -m #{controller_file}])
-          else
-            tracers.any? { |tracer| tracer =~ %r{\A([./\w]+):\d+:in `[\w\s]+'\z} }
-            file_name = Regexp.last_match[1]
-            Parser::Runner::RubyRewrite.go(%W[-l #{default_corrector} -m #{file_name}])
-          end
+          BulletmarkRepairer::Pathcer.execute(
+            controller: env['action_dispatch.request.parameters']['controller'],
+            action: env['action_dispatch.request.parameters']['action'].to_sym
+          )
         end
       end
     end
---
+++ lib/bulletmark_repairer/patcher.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'parser/runner/ruby_rewrite'
+
+module BulletmarkRepairer
+  class Pathcer
+    def self.execute(controller:, action:)
+      new(controller:, action:).execute
+    end
+
+    def execute
+      tracers = BulletmarkRepairer.notifications.last.instance_variable_get(:@callers)
+      n_plus_one_in_view = tracers.any? { |tracer| tracer.match?(%r{\A#{Rails.root}/app/views/[./\w]+:\d+:in `[\w]+'\z}) }
+
+      if n_plus_one_in_view
+        Parser::Runner::RubyRewrite.go(%W[-l #{controller_corrector} -m #{controller_file}])
+      else
+        tracers.any? { |tracer| tracer =~ %r{\A([./\w]+):\d+:in `[\w\s]+'\z} }
+        file_name = Regexp.last_match[1]
+        Parser::Runner::RubyRewrite.go(%W[-l #{default_corrector} -m #{file_name}])
+      end
+    end
+
+    private
+
+    def initialize(controller:, action:)
+      @controller = controller
+      BulletmarkRepairer.action = action
+    end
+
+    def controller_file
+      @controller_file ||= "#{Rails.root}/app/controllers/#{@controller}_controller.rb"
+    end
+
+    def controller_corrector
+      @controller_corrector ||= Pathname.new(__FILE__).sub('/patcher.rb', '/controller_corrector.rb')
+    end
+
+    def default_corrector
+      @default_corrector ||= Pathname.new(__FILE__).sub('/patcher.rb', '/corrector.rb')
+    end
+  end
+end
