07feae1b0837b537b146fe1d4fcde74b0fcccc55
makicamel <unright@gmail.com>
2023-10-08 19:47:26 +0900

    :wave: Revert "Introduce CallerCollector"
    This reverts commit 2769d420ef568f355bf627349311138a45fbd626.

--- lib/bulletmark_repairer/railtie.rb
+++ lib/bulletmark_repairer/railtie.rb
@@ -9,10 +9,5 @@ module BulletmarkRepairer
       require 'bulletmark_repairer/rack'
       app.middleware.insert_after Bullet::Rack, BulletmarkRepairer::Rack
     end
-
-    ActiveSupport.on_load(:active_record) do
-      require 'bulletmark_repairer/monkey_patches/active_record/core'
-      ::ActiveRecord::Core::ClassMethods.prepend BulletmarkRepairer::ActiveRecord::Core
-    end
   end
 end
